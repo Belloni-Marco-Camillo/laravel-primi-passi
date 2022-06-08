9c9e4f6ae12bb6c3740deb83bb80e79fa3ec28c0
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-06-08 15:38:59 +0200

    end_commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+route::get('/', function(){
+    $text = 'hello world';
+    $name = 'marco camillo belloni';
+    return view('home', compact('text','name'));
+});
+
+
+route::get('/jobs', function(){
+    $jobs = 'web designer | web developer';
+    return view('jobs', compact('jobs'));
 });
